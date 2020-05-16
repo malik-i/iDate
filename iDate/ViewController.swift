0c65c7ab0ec527d48e484cbcf4ad4c5c3400cf19
malik-i <malikiscanedu@gmail.com>
2020-05-15 20:19:46 -0700

    initial commit

---
+++ iDate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDate
+//
+//  Created by Malik Iscandari on 5/15/20.
+//  Copyright © 2020 Malik Iscandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
